2aa2daa16fa0aeed7cdb50ecafd12cbe979de459
Lucas <lucasoliveirag@gmail.com>
2023-10-26 15:41:14 -0300

    build: criação da classe residentedto

--- trabalhoIndividual/src/main/java/br/com/api/trabalhoIndividual/DTO/ResidenteDTO.java
+++ trabalhoIndividual/src/main/java/br/com/api/trabalhoIndividual/DTO/ResidenteDTO.java
@@ -1,5 +1,27 @@
 package br.com.api.trabalhoIndividual.DTO;
 
 public class ResidenteDTO {
-
+	
+	private String telefone;
+	private String usuario;
+	private String cpf;
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
 }
