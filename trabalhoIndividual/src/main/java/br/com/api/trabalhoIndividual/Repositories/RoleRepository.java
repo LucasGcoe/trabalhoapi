92b5bd71785af4502789d6f691efbdc145375d7a
LucasGcoe <lucasoliveirag@gmail.com>
2023-10-26 12:00:16 -0300

    feat: Implementando Role e User Repository

--- trabalhoIndividual/src/main/java/br/com/api/trabalhoIndividual/Repositories/RoleRepository.java
+++ trabalhoIndividual/src/main/java/br/com/api/trabalhoIndividual/Repositories/RoleRepository.java
@@ -1,5 +1,24 @@
 package br.com.api.trabalhoIndividual.Repositories;
 
-public class RoleRepository {
+import java.util.Optional;
+import java.util.Set;
 
+import javax.management.relation.Role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import br.com.api.trabalhoIndividual.Enums.TipoRoleEnum;
+
+@Repository
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+	Optional<Role> findByName(TipoRoleEnum roleUser);
+
+	@Query(value = "select r.* from residenteTeste u \rzn"
+			+ "inner join residenteRole ur on u.id_residente = ur.residente_id\r\n"
+			+ "inner join roles r on ur.role_id = r.id\r\n"
+			+ "where u.email_residente = :email", nativeQuery=true)
+	Set<Role> roles (String email);
 }
