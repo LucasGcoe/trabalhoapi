6a48535bfe4411c4d11c662b7b30a89b155f34cf
Lucas <lucasoliveirag@gmail.com>
2023-10-26 13:03:43 -0300

    feat: implementando classe MessageResponseDTO

--- trabalhoIndividual/src/main/java/br/com/api/trabalhoIndividual/DTO/MessageResponseDTO.java
+++ trabalhoIndividual/src/main/java/br/com/api/trabalhoIndividual/DTO/MessageResponseDTO.java
@@ -2,8 +2,32 @@ package br.com.api.trabalhoIndividual.DTO;
 
 public class MessageResponseDTO {
 
-	public MessageResponseDTO(String string) {
-		// TODO Auto-generated constructor stub
+	private String message;
+	//private String token;
+
+	public MessageResponseDTO(String message, String token) {
+		super();
+		this.message = message;
+		//this.token = token;
 	}
 
+//	public String getToken() {
+//		return token;
+//	}
+//
+//	public void setToken(String token) {
+//		this.token = token;
+//	}
+
+	public MessageResponseDTO(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
 }
